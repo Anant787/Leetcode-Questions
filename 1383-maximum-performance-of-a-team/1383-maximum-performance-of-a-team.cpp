0ba45321c0e81dc8516fc58f0f6f00616c0ea7cf
Anant gupta <91842337+Anant787@users.noreply.github.com>
2022-09-11 16:25:08 +0530

    Time: 167 ms (32.75%), Space: 36.9 MB (56.16%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -2,31 +2,35 @@ class Solution {
 public:
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
         
-        vector<pair<int,int>>v;
-        priority_queue<int,vector<int>,greater<int>>pq;
+        vector<pair<int,int>>v;                  //Pair
         
-        for(int i=0;i<n;i++)
+        priority_queue<int,vector<int>,greater<int>>pq;   //for min heap giveout minimum
+        
+        for(int i=0;i<n;i++)       //element push in pair of vector
         {
-            v.push_back({efficiency[i],speed[i]});
+            v.push_back({efficiency[i],speed[i]}); 
         }
         
-        long sum=0,ans=0;
-        sort(v.begin(),v.end());
+        long sum=0,ans=0; 
+        
+        sort(v.begin(),v.end());  //sort the vector according to efficiency
         
         for(int i=n-1;i>=0;i--)
         {
-            sum+=v[i].second;
-            pq.push(v[i].second);
+            sum+=v[i].second;        //total sum according to max sum
+            pq.push(v[i].second);    // sum number is to be pushed into PQ
             
-            if(pq.size()>k)
+            if(pq.size()>k)         //only  need two engineers(k=2)
             {
-                sum-=pq.top();
-                pq.pop();
+                sum-=pq.top();     //if true then subtract the element form sum
+                pq.pop();         //and Delete that element
             }
             
-            ans=max(ans,sum*v[i].first);
+            ans=max(ans,sum*v[i].first);   // find max the performance by multiple by efficiency
         }
-        return ans % 1000000007;
+        
+        
+        return ans % 1000000007;      //for some input
         
     }
 };
