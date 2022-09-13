96b575b1dd3de34e1fc56e4b1432e432f9451577
Anant gupta <91842337+Anant787@users.noreply.github.com>
2022-09-13 10:41:27 +0530

    Time: 27 ms (28.68%), Space: 14.1 MB (38.65%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        
+        int count = 0;
+        for (auto c : data) {
+            if (count == 0) {
+                if ((c >> 5) == 0b110) count = 1;
+                else if ((c >> 4) == 0b1110) count = 2;
+                else if ((c >> 3) == 0b11110) count = 3;
+                else if ((c >> 7)) return false;
+            } else {
+                if ((c >> 6) != 0b10) return false;
+                count--;
+            }
+        }
+        return count == 0;
+        
+        // int rem=0;
+        // for(auto &x : data)
+        // {
+        //     if(rem==0)
+        //     {
+        //         if((x>>5) == 0b110)
+        //         {
+        //             rem=1;
+        //         }
+        //         else if((x>>4) == 0b1110)
+        //         {
+        //             rem=2;
+        //         }
+        //         else if((x>3) == 0b11110)
+        //         {
+        //             rem=3;
+        //         }
+        //         else if((x>>7)!=0)
+        //         {
+        //             return false;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         if((x>>6)!=0b10)
+        //         {
+        //             return false;
+        //         }
+        //         else
+        //         {
+        //             rem--;
+        //         }
+        //     }
+        // }
+        // return rem==0;
+    }
+};
