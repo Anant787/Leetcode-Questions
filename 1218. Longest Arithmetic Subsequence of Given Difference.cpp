cc594b1c1e85c941adcbbfa81eb9b5b1f245d59e
Anant gupta <91842337+Anant787@users.noreply.github.com>
2023-07-14 12:56:44 +0530

    Create 1218. Longest Arithmetic Subsequence of Given Difference.cpp

---
+++ 1218. Longest Arithmetic Subsequence of Given Difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    //Map Solution
+    int longestSubsequence(vector<int>& arr, int difference) {
+        unordered_map<int,int> m;
+        int ans=1;
+        for(int i=arr.size()-1;i>=0;i--){
+            m[arr[i]]=max(m[arr[i]],m[arr[i]+difference]+1);
+            ans=max(ans,m[arr[i]]);
+        }
+        return ans;
+    }
+};
