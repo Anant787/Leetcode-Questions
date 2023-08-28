8c874d241cd4d4be5418c05d29cc1ef51f9748d4
Anant gupta <91842337+Anant787@users.noreply.github.com>
2023-08-28 12:37:38 +0530

    Create 225. Implement Stack using Queues.cpp

---
+++ 225. Implement Stack using Queues.cpp
@@ -0,0 +1,35 @@
+class MyStack {
+public:
+    deque<int> dq;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        dq.push_front(x);
+    }
+    
+    int pop() {
+        int element = dq.front();
+        dq.pop_front();
+        return element;
+    }
+    
+    int top() {
+        return dq.front();
+    }
+    
+    bool empty() {
+        return dq.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
