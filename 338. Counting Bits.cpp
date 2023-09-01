f8ba3d257636891b862f546389cf58b3b0485bd7
Anant gupta <91842337+Anant787@users.noreply.github.com>
2023-09-01 12:48:49 +0530

    Create 338. Counting Bits.cpp

---
+++ 338. Counting Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    //Dp solution
+    vector<int> countBits(int n) {
+        vector<int> answer(n + 1);
+        
+        for (int i=1; i<n+1; i++) {
+            answer[i] = answer[i/2] + (i%2) ;
+        }
+
+        return answer ;
+    }
+};
