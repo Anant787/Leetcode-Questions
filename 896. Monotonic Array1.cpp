bfe8db5ade38a786b26d350d9fe70225f5d2be7c
Anant gupta <91842337+Anant787@users.noreply.github.com>
2023-09-29 11:49:54 +0530

    Create 896. Monotonic Array1.cpp

---
+++ 896. Monotonic Array1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int cnt = 0;  // flag count to check the condition
+
+        for(int i =1;i<nums.size();i++)
+        {
+            if(cnt == 0 && nums[i] < nums[i-1])
+            {
+                cnt = -1;   // check decreasing
+            }
+            else if(cnt == 0 && nums[i] > nums[i-1] )
+            {
+                cnt = 1;   // check increasing
+            }
+
+            else if(cnt == -1 && nums[i] > nums[i-1]) //check the contradict conditions
+            {
+                return false;
+            }
+            else if(cnt ==1 && nums[i] < nums[i-1])
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
