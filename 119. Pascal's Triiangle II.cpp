b76a511dcfe7f3c8aba8a4e7c87f3314eab39a78
Anant gupta <91842337+Anant787@users.noreply.github.com>
2023-10-16 10:58:29 +0530

    Create 119. Pascal's Triiangle II.cpp

---
+++ 119. Pascal's Triiangle II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+        vector<int>ans(rowIndex+1,1);       //Vector to store the elememts
+        
+        
+        for(int i=1;i<ans.size()-1;i++)
+        {
+            for(int j=rowIndex-1;j>=i;j--)
+            {
+                ans[j]=ans[j+1]+ans[j];
+            }
+        }
+        return ans;
+        
+    }
+};
