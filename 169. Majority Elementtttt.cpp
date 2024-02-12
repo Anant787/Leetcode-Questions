5b5cb83fa0325777c69973822dc329e52f0bee33
Anant gupta <91842337+Anant787@users.noreply.github.com>
2024-02-12 15:23:21 +0530

    Create 169. Majority Elementtttt.cpp

---
+++ 169. Majority Elementtttt.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int el;
+        int count=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count==0)
+            {
+                count=1;
+                el = nums[i];
+            }
+            else if(nums[i] == el)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+
+        int count1 = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] == el)
+            {
+                count1++;
+            }
+        }
+        if(count1 > (nums.size() / 2))
+        {
+            return el;
+        }
+
+        return -1;
+        
+       // sort(nums.begin(),nums.end());
+        
+        // int count=0;
+        // int temp;
+        
+        // for(int i = 0;i<nums.size();i++)
+        // {
+        //     if(count)
+        //     {
+        //         count+=(nums[i]==temp?1:-1);
+        //     }
+        //     else
+        //     {
+        //         temp = nums[i];
+        //         count = 1 ;
+        //     }
+        // }
+        // return temp;
+    }
+};
