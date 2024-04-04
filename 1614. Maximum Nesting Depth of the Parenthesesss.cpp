0f3d8a7cbe48607db3bdf4394e822e88edc4f04f
Anant gupta <91842337+Anant787@users.noreply.github.com>
2024-04-04 16:57:28 +0530

    Create 1614. Maximum Nesting Depth of the Parenthesesss.cpp

---
+++ 1614. Maximum Nesting Depth of the Parenthesesss.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char>st;
+        int ans=0;
+        int currmax=0,max_depth=0;
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i] == '(')
+            {
+                st.push(s[i]);
+                currmax++;
+               // ans = ans > st.size() ? 0 : st.size(); 
+            }
+
+            else if(s[i] == ')')
+            {
+                st.pop();
+                if(currmax>max_depth)
+                {
+                    max_depth = currmax;
+                }
+                currmax--;
+            }
+        }
+        return max_depth;
+    }
+};
