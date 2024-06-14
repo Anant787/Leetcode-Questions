9c3bdf65bef52d3e60029326d1e5dd3b10daf199
Anant gupta <91842337+Anant787@users.noreply.github.com>
2024-06-14 11:50:13 +0530

    Create 945. Minimum Increment to Make Array Unique.cpp

---
+++ 945. Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        int ans = 0;
+
+        for(int i = 1 ; i < nums.size() ; i++){
+            if(nums[i - 1] >= nums[i]){
+                int diff = nums[i - 1] - nums[i] + 1;
+                nums[i] += diff;
+                ans += diff;
+            }
+            
+        }
+
+        return ans;
+    }
+};
